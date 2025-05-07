11aa95c195027c9d7dd173f4e138369fc815fc51
Oleksandr Siliaiev <tortikteo@gmail.com>
2025-05-07 09:47:55 +0300

    anxiety-test

--- src/data/tests.ts
+++ src/data/tests.ts
@@ -52,5 +52,56 @@ export const TESTS: Test[] = [
             ]
         },
     ]
+  },
+  {
+    id: 'anxiety-test',
+    title: 'Тест на тревожность',
+    description: 'Оценка эмоционального состояния',
+    questions: [
+      {
+        id: 1,
+        text: "Как часто вы чувствуете тревогу?",
+        answers: [
+          { id: 1, text: "Никогда", score: 0 },
+          { id: 2, text: "Иногда", score: 1 },
+          { id: 3, text: "Часто", score: 2 }
+        ]
+      },
+        {
+            id: 2,
+            text: "Как часто вы теряете интерес к вещам, которые раньше вам нравились?",
+            answers: [
+            { id: 1, text: "Никогда", score: 0 },
+            { id: 2, text: "Иногда", score: 1 },
+            { id: 3, text: "Часто", score: 2 },
+            ]
+        },
+        {
+            id: 3,
+            text: "Как часто вы чувствуете усталость или недостаток энергии?",
+            answers: [
+            { id: 1, text: "Никогда", score: 0 },
+            { id: 2, text: "Иногда", score: 1 },
+            { id: 3, text: "Часто", score: 2 }
+            ]
+        },
+        {
+            id: 4,
+            text: "test4",
+            answers: [
+            { id: 1, text: "Никогда", score: 0 },
+            { id: 2, text: "Иногда", score: 1 },
+            { id: 3, text: "Часто", score: 2 }
+            ]
+        },{
+            id: 5,
+            text: "test5",
+            answers: [
+            { id: 1, text: "Никогда", score: 0 },
+            { id: 2, text: "Иногда", score: 1 },
+            { id: 3, text: "Часто", score: 2 }
+            ]
+        },
+    ]
   }
 ]
