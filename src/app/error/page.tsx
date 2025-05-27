2329f619a4c2e89733221de6e2d20174fc463cb7
Oleksandr Siliaiev <tortikteo@gmail.com>
2025-05-27 15:40:50 +0300

    error local

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -5,13 +5,13 @@ export default function ErrorPage() {
     return (
       <div className="min-h-screen flex items-center justify-center">
         <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">Ошибка 500</h1>
-          <p className="text-lg">Произошла непредвиденная ошибка</p>
+          <h1 className="text-4xl font-bold mb-4">Error 500</h1>
+          <p className="text-lg">Виникла неочікувана помилка</p>
           <Link 
             href="/" 
             className="mt-4 inline-block text-blue-500 hover:underline"
           >
-            Вернуться на главную
+            Повернутися на головну
           </Link>
         </div>
       </div>
